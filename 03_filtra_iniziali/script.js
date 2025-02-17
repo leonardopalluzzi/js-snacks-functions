77f04fad6bcfe7908dbcbfe5f06d6ea6f5c73dd1
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-17 15:19:24 +0100

    third snack complete

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,6 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const searchByLetter = (arr, letter) => {
+    const finder = [];
+    let j = 0;
+    for (let i = 0; i < arr.length; i++){
+        const currentLetter = arr[i].toLowerCase();
+        if(currentLetter[0] == letter){
+            finder[j] = currentLetter;
+            j++;
+        }
+    }
+    return finder;
+}
+
+const userLetter = prompt("inserisci l'iniziale da cercare");
+console.log(searchByLetter(names, userLetter));
 
 
 // Invoca la funzione qui e stampa il risultato in console
