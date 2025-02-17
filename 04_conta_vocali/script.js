46fccf2da590b687e19091b4a4681d297509fc38
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-17 15:26:01 +0100

    fourth snack complete

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,6 +4,19 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const vowels = string => {
+    let j = 0;
+    string.toLowerCase();
+    for (let i = 0; i < string.length; i++){
+        if(string[i] == 'a' || string[i] == 'e' || string[i] == 'i' || string[i] == 'o' || string[i] == 'u'){
+            j++
+        }
+    }
+    return j;
+}
+
+console.log(`nella parola inserita sono presenti ${vowels(word)} vocali`);
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
