50b4bd50e5f4632d1b94be2deed129352cfc5ad4
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-17 15:08:56 +0100

    second snack complete

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+const initialList = arr => {
+    const initialArray = [];
+    for(let i = 0; i < arr.length; i++){
+        const firstLetter = arr[i];
+        initialArray.push(firstLetter[0]);
+    }
+    return initialArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(initialList(names));
 
+// SOLUZIONE ABBREVIATA
+const shortInitialList = arr => arr.map((index) => index[0]).join('');
 
+console.log(initialList(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
